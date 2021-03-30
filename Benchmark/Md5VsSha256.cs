ccf5226dc2ef69048fde23315d6fcd724788915f
Mohammed Tanbir Hossain <ovibhuiyan43@gmail.com>
2021-03-30 10:42:19 +0900

    added more benchmarkes

---
+++ Benchmark/Md5VsSha256.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System;
+using System.Security.Cryptography;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    //[MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
+    public class Md5VsSha256
+    {
+        private const int N = 10000;
+        private readonly byte[] data;
+
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly MD5 md5 = MD5.Create();
+
+        public Md5VsSha256()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
+
+        [Benchmark]
+        public string Array()
+        {
+            var arr = new string[] { "aa", "bb" };
+            return arr[0];
+        }
+        [Benchmark]
+        public string OnlyString()
+        {
+            return "ovi";
+        }
+    }
+}
